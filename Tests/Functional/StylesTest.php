59096c42f96eca266c72099e1f02d99cbc52a958
Sebastian Helzle <sebastian@helzle.it>
2019-11-09 18:37:19 +0100

    TASK: Temporary workaround for travis ci not being able to run tests

--- Tests/Functional/StylesTest.php
+++ Tests/Functional/StylesTest.php
@@ -7,11 +7,26 @@ namespace Shel\CriticalCSS\Tests\Functional;
  * This file is part of the Shel.CriticalCSS package.
  */
 
+use Neos\Fusion\Tests\Functional\FusionObjects\AbstractFusionObjectTest;
+use Neos\Fusion\View\FusionView;
+
 /**
  * Testcase for the styles object
  */
-class StylesTest extends AbstractStyleTest
+class StylesTest extends AbstractFusionObjectTest
 {
+    /**
+     * @inheritDoc
+     */
+    protected function buildView(): FusionView
+    {
+        $view = parent::buildView();
+        $view->setPackageKey('Shel.CriticalCSS');
+        $view->setFusionPathPattern(__DIR__ . '/Fixtures/Fusion');
+        $view->assign('fixtureDirectory', __DIR__ . '/Fixtures/');
+        return $view;
+    }
+
     /**
      * @test
      */
