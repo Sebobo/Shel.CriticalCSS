28b87890d9ae54ac94b71deee305c504d9b21fbe
Sebastian Helzle <sebastian@helzle.it>
2019-11-09 17:34:00 +0100

    BUGFIX: Inlined method from 3rd party package

--- Classes/FusionObjects/LoadStylesImplementation.php
+++ Classes/FusionObjects/LoadStylesImplementation.php
@@ -10,19 +10,12 @@ namespace Shel\CriticalCSS\FusionObjects;
 use Neos\Flow\Annotations as Flow;
 use Neos\FluidAdaptor\Core\ViewHelper\Exception\InvalidVariableException;
 use Neos\Fusion\FusionObjects\AbstractFusionObject;
-use Shel\NeosBase\Service\ResourceService;
 
 /**
  * Adds styles from the specified file and inserts them inline
  */
 class LoadStylesImplementation extends AbstractFusionObject
 {
-    /**
-     * @Flow\Inject
-     * @var ResourceService
-     */
-    protected $resourceService;
-
     /**
      * HTML content that should be processed (optional)
      *
@@ -49,11 +42,28 @@ class LoadStylesImplementation extends AbstractFusionObject
     public function evaluate(): string
     {
         try {
-            $styles = $this->resourceService->loadResourceContent($this->getPath());
+            $styles = $this->loadResourceContent($this->getPath());
         } catch (InvalidVariableException $e) {
             throw new \InvalidArgumentException('Resource ' . $this->getPath() . ' cannot be loaded!', 1573314641);
         }
 
         return '<style data-inline>' . $styles . '</style>' . ($this->getContent() ?? '');
+    }
+
+    /**
+     * Loads the content of a resource and returns it as string
+     *
+     * @param string $path
+     * @param string
+     * @return string Content of the resource
+     * @throws InvalidVariableException
+     */
+    public function loadResourceContent($path): string
+    {
+        if ($path === null) {
+            throw new InvalidVariableException('You have to define a path.', 1573317165);
+        }
+
+        return file_get_contents($path);
     }
 }
