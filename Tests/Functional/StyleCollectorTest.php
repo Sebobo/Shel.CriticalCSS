b83b8d5c51aded332d574a73153620c743904a24
Sebastian Helzle <sebastian@helzle.it>
2019-11-09 16:39:59 +0100

    TASK: Add test for stylecollector

---
+++ Tests/Functional/StyleCollectorTest.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace Shel\CriticalCSS\Tests\Functional;
+
+/*
+ * This file is part of the Shel.CriticalCSS package.
+ */
+
+use Neos\Fusion\Tests\Functional\FusionObjects\AbstractFusionObjectTest;
+
+/**
+ * Testcase for the style collector object
+ */
+class StyleCollectorTest extends AbstractFusionObjectTest
+{
+    protected function buildView()
+    {
+        $view = parent::buildView();
+        $view->setPackageKey('Shel.CriticalCSS');
+        $view->setFusionPathPattern(__DIR__ . '/Fixtures/Fusion');
+        $view->assign('fixtureDirectory', __DIR__ . '/Fixtures/');
+        return $view;
+    }
+
+    /**
+     * @test
+     */
+    public function collectMultipleStylesWorks()
+    {
+        $view = $this->buildView();
+
+        $view->setFusionPath('styleCollector/collectMultipleStyles');
+        $this->assertEquals(
+            '
+        <html>
+            <head>
+                <title>Test</title>
+            <style>.foo{color:blue}.bar{color:green}</style></head>
+            <body>
+                
+                <div class="foo">foo</div>
+                
+                <div class="bar">bar</div>
+            </body>
+        </html>', $view->render());
+    }
+
+    /**
+     * @test
+     */
+    public function removeDuplicatesStylesWorks()
+    {
+        $view = $this->buildView();
+
+        $view->setFusionPath('styleCollector/removeDuplicatesStyles');
+        $this->assertEquals(
+            '
+        <html>
+            <head>
+                <title>Test</title>
+            <style>.foo{color:blue}</style></head>
+            <body>
+                
+                <div class="foo">foo</div>
+                
+                <div class="foo">foo</div>
+            </body>
+        </html>', $view->render());
+    }
+}
